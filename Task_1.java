3526b74ff6d4bf22aa9aebb824f036060d8be41e
Tori Vorobei <whispa.vi@gmail.com>
2022-03-26 21:45:35 +0300

    hw

---
+++ Task_1.java
@@ -0,0 +1,16 @@
+public class Person {
+    private static String[] args;
+
+    public static void main(String[] args) {
+          int yob = 1990;
+          int yob_s = 2001;
+
+
+
+          System.out.println("Если человек родился в " + yob + ", то его возраст - разница между 2020 и " + yob);
+          System.out.println("Если человек родился в " + yob_s + ", то его возраст - разница между 2020 и " + yob_s);
+
+
+    }
+}
+
---
+++ Task_1_1.java
@@ -0,0 +1,22 @@
+public class Student {
+   private static String[] args;
+
+        public static void main(String[] args) {
+            String firstName = "Вова Сидоров";
+            String level = "Ученик 5-го класса";
+            int age = 10;
+            String line = "..........>-(:........";
+
+            String firstName_s = "Маша Семенова";
+            String level_s = "Ученица 9-го класса";
+            int age_s = 14;
+            String line_s = "........>>(:.........";
+
+            System.out.println(firstName);
+            System.out.println(level);
+            System.out.println(line);
+            System.out.println(firstName_s);
+            System.out.println(level_s);
+            System.out.println(line_s);
+        }
+    }
